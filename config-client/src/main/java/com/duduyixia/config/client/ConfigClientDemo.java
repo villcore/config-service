545b60f9501a7b3217ada2e48214eddcc0612b6e
wangtao <villcore@163.com>
2019-11-07 19:18:04 +0800

    完成测试。

---
+++ config-client/src/main/java/com/duduyixia/config/client/ConfigClientDemo.java
@@ -0,0 +1,28 @@
+package com.duduyixia.config.client;
+
+/**
+ * created by WangTao on 2019-09-28
+ */
+public class ConfigClientDemo {
+    public static void main(String[] args) throws ConfigException, InterruptedException {
+        // set property
+        System.setProperty("config.client.namespace", "duduyixia");
+        System.setProperty("config.client.failover", "true");
+
+        // create config service
+        ConfigService configService = ConfigServiceFactory.createConfigService();
+
+        // listener
+        configService.addListener("test", config -> {
+            System.out.println("config -> " + config);
+        });
+
+        // get config
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(configService.getConfig("test"));
+            Thread.sleep(1000);
+        }
+
+        Thread.sleep(1000 * 1000L);
+    }
+}
